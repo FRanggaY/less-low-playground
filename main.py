965fe21a397240f379059b32d9a01ad6b28e6dd2
FRanggaY <franciscusrangga@gmail.com>
2024-08-30 22:07:00 +0700

    update : add cors

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+# whitelist alloed routes
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+]
+
+app = FastAPI(
+    title="Less Low Playground"
+)
+
+# cors middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def check_status():
